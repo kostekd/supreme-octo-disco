691b68e2b8303c4d9dde16794c91db1e1d999147
Dominik Kostencki <dominikkostencki99@gmail.com>
2022-05-15 11:00:24 +0200

    add unit tests

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,2 +1,26 @@
+import { render, screen } from "@testing-library/react";
 import React from "react";
+import {Button, ButtonProps} from './Button';
 
+
+describe('Button', () => {
+    let initProps: ButtonProps = {
+        label: 'Foo',
+        size: 'large'
+    }
+    
+    test('should render content on Button', () => {
+        render(<Button {...initProps}/>);
+        expect(screen.getByText(initProps.label)).toBeInTheDocument();
+    })
+
+    test('should call onClick', () => {
+        const onClick = jest.fn();
+        initProps = {...initProps, onClick}
+        render(<Button {...initProps}/>);
+
+        screen.getByText(initProps.label).click()
+
+        expect(onClick).toBeCalledTimes(1);
+    })
+});
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import s from "./Button.module.scss";
 import cx from "classnames";
+
 export type ButtonSize = "small" | "medium" | "large";
 
 export interface ButtonProps {
+  /**
+   * What label should be displayed on the button
+   */
   label: string;
+  /**
+   * What size should the button be?
+   */
   size: ButtonSize;
+  /**
+   * What should happend when button is being clicked?
+   */
   onClick?: () => void;
 }
+
+/**
+ * 
+ * @author Dominik Kostencki 
+ * @description Primary button UI component
+ */
 
 const Button = ({ label, size = "medium", onClick }: ButtonProps) => {
   return (
