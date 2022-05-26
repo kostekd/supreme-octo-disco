5362eb9d587c85b33ab683dec8c10ad27ed0fd8a
Dominik <dominikkostencki99@gmail.com>
2022-05-26 19:50:45 +0200

    configure and mess around with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './store';
+import { logIn, logOut } from './store/reducers/authReducer/actions';
+import { add } from './store/reducers/counterReducer/actions';
 
 function App() {
+  const authentication = useSelector((state: RootState) => state.authentication);
+  const counter = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
+
+  const logInFunction = () => {
+    dispatch(logIn());
+  }
+  const logOutFunction = () => {
+    dispatch(logOut());
+  }
+
+  const toggleNumber = (value: number) => {
+    dispatch(add(value));
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +36,14 @@ function App() {
         >
           Learn React
         </a>
-        <div className='bg-black'> Essa byku </div>
+        {authentication.isLoggedIn && <div> IsLoggedIn </div>}
+        <button onClick={logInFunction}>Log In</button>
+        <button onClick={logOutFunction}>Log Out</button>
+        <button onClick={() => toggleNumber(5)}>Add 5</button>
+        <button onClick={() => toggleNumber(10)}>Add 10</button>
+        <button onClick={() => toggleNumber(-10)}>Subtract 10</button>
+        <h4>{counter.value}</h4>
+        
       </header>
     </div>
   );
---
+++ src/store/rootReducer.tsx
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import authenticationReducer from "./reducers/authReducer/reducer";
+import counterReducer from "./reducers/counterReducer/reducer";
+
+export const rootReducer = combineReducers({
+    authentication: authenticationReducer,
+    counter: counterReducer,
+});
