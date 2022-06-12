a223240f1816b3bfd63a756c7b5690cbffe6defe
Dominik <dominikkostencki99@gmail.com>
2022-06-12 19:05:53 +0200

    documentation

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -6,22 +6,38 @@ import ReactDOM from "react-dom";
 import { AiOutlineClose } from "react-icons/ai";
 
 interface ModalProps {
+  /**
+   * Current state of modal visible/hidden
+   */
   isOpen: boolean;
+  /**
+   * Toggles the visibility of Modal
+   */
   handleModal: () => void;
+  /**
+   * Id of a div that should render Modal (default is placed in index.html)
+   */
+  destination?: string;
+  /**
+   * Standard children component
+   */
   children: React.ReactNode;
 }
 
-const Modal = ({ isOpen, handleModal, children }: ModalProps) => {
-  const portalDestination = document.querySelector("#modal-root");
+/**
+ *
+ * @author Dominik Kostencki
+ * @description Primary modal Component
+ */
+const Modal = ({ isOpen, handleModal, destination, children }: ModalProps) => {
+  const portalDestination = document.querySelector(destination || "#modal-root");
 
   if (portalDestination && isOpen) {
     return ReactDOM.createPortal(
       <div className={s.modal}>
         <div className={cx(s["modal--content"])}>
           <div className={cx(s["modal--content--button"])}>
-            <button onClick={handleModal}>
-              <AiOutlineClose />
-            </button>
+              <AiOutlineClose onClick={handleModal} className={s['icon']} />
           </div>
           {children}
         </div>
