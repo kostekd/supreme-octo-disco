ab7b244b0f1b601e13b895ea1b4b6160ecae08db
Dominik <dominikkostencki99@gmail.com>
2022-07-22 20:28:17 +0200

    dev: initialize empty service

--- src/store/reducers/counterReducer/reducer.ts
+++ src/store/reducers/counterReducer/reducer.ts
@@ -2,18 +2,17 @@ import { createReducer } from "@reduxjs/toolkit";
 import { add } from "./actions";
 
 interface counterState {
-    value: number,
-};
-
-const initialState: counterState = {
-    value: 0,
+  value: number;
 }
 
+const initialState: counterState = {
+  value: 0,
+};
+
 const counterReducer = createReducer(initialState, (builder) => {
-    builder
-    .addCase(add, (state, action) => {
-        state.value += action.payload.number;
-    })
-})
+  builder.addCase(add, (state, action) => {
+    state.value += action.payload.number;
+  });
+});
 
 export default counterReducer;
